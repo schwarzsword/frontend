a325e67889fc20c40cefedc58d82b0ca2ce81e05
schwarzkv <schwarsword@gmail.com>
2019-07-10 13:50:03 +0300

    pre-final

--- src/containers/MyTable/index.js
+++ src/containers/MyTable/index.js
@@ -1,10 +1,12 @@
 import React, {PureComponent} from "react";
 import "./index.css";
-import {Button} from "react-bootstrap";
-import {MDBDataTable, MDBBtn} from 'mdbreact';
+import {FormGroup, FormControl, Form, Button} from "react-bootstrap";
 import axios from "axios";
 import {urlPort} from "../../index";
 import {DropdownButton} from "react-bootstrap";
+
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 Date.prototype.ddmmyyyy = function () {
     var mm = this.getMonth() + 1; // getMonth() is zero-based
@@ -18,12 +20,9 @@ export default class MyTable extends PureComponent {
         super(props);
 
         this.state = {
-            data: {
-                columns: [],
-                rows: []
-            },
-            colors: [],
-            loaded: null
+            data: [],
+            loaded: [],
+            search: ""
         };
     }
 
@@ -40,38 +39,34 @@ export default class MyTable extends PureComponent {
     };
 
     showFiltered = tag => {
-        let list = document.getElementsByTagName("tr");
-        for (let i = 1; i < list.length - 1; i++) {
-            list[i].style.display = "none";
-        }
+        let temp = [];
         if (tag === "any") {
-            for (let i = 1; i < list.length - 1; i++) {
-                list[i].style.display = "";
-            }
+            temp = this.state.loaded;
         } else {
-            let list2 = document.getElementsByClassName(tag);
-            for (let i = 0; i < list2.length; i++) {
-                list2[i].style.display = "";
+            for (let i = 0; i < this.state.loaded.length; i++) {
+                if (this.state.loaded[i].color === tag) {
+                    temp.push(this.state.loaded[i]);
+                }
             }
         }
+        this.setState({data: temp})
     };
 
     setRows = res => {
         let rows = [];
         for (let i = 0; i < res.data.length; i++) {
             let doc = res.data[i];
-            let st = "common";
+            let st = "white";
             if (doc.isExecuted)
-                st = "green";
+                st = "#d4fa9b";
             else {
-                if (new Date(new Date(doc.executionDate) - new Date()).getDate() <= 7)
-                    st = "red";
+                if (new Date(doc.executionDate) - new Date() <= 604800000 && new Date(doc.executionDate) - new Date() >= 0)
+                    st = "#fcbf44";
                 else {
-                    if (new Date(doc.executionDate) - new Date() <= 0)
-                        st = "dated";
+                    if (new Date(doc.executionDate) - new Date() < 0)
+                        st = "#fc4444";
                 }
             }
-            this.state.colors.push(st);
             rows.push({
                 receiving_date: new Date(doc.receivingDate).ddmmyyyy(),
                 court: doc.courtByCourtName.name,
@@ -79,97 +74,152 @@ export default class MyTable extends PureComponent {
                 plaintiff: doc.plaintiff,
                 defendant: doc.defendant,
                 execution_date: new Date(doc.executionDate).ddmmyyyy(),
-                edit: <MDBBtn color="crimson" onClick={() => {
+                edit: <Button onClick={() => {
                     this.edit(doc)
-                }} rounded size="sm">Изменить</MDBBtn>,
+                }}>Изменить</Button>,
+                color: st
             })
         }
         return rows;
     };
 
 
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+        let rows = [];
+        let docs = this.state.loaded;
+        let pattern = event.target.value;
+        for (let i = 0; i < docs.length; i++) {
+            if (
+                docs[i].receiving_date.includes(pattern) ||
+                docs[i].court.includes(pattern) ||
+                docs[i].judge.includes(pattern) ||
+                docs[i].plaintiff.includes(pattern) ||
+                docs[i].defendant.includes(pattern) ||
+                docs[i].execution_date.includes(pattern)
+            )
+                rows.push(docs[i]);
+        }
+        this.setState({data: rows})
+
+    }
+
     componentDidMount() {
-        let columns = [
-            {
-                label: 'Дата получения',
-                field: 'receiving_date',
-                width: 50
-            },
-            {
-                label: 'Наименование суда',
-                field: 'court',
-                width: 150
-            },
-            {
-                label: 'Судья (ФИО)',
-                field: 'judge',
-                width: 150
-            },
-            {
-                label: 'Истец',
-                field: 'plaintiff',
-                width: 100
-            },
-            {
-                label: 'Ответчик',
-                field: 'defendant',
-                width: 100
-            },
-            {
-                label: 'Дата исполнения',
-                field: 'execution_date',
-                width: 100
-            },
-            {
-                label: 'Изменение',
-                field: 'edit',
-                width: 100
-            },
-        ];
         let rows = [];
         axios.get(urlPort('/show'), {withCredentials: true})
             .then(
                 res => {
                     rows = this.setRows(res);
-                    let temp = {columns, rows};
-                    this.setState({data: temp});
-                    let list = document.getElementsByTagName("tr");
-                    for (let i = 0; i < this.state.colors.length; i++) {
-                        list[i + 1].className = this.state.colors[i];
-                    }
+                    this.setState({data: rows});
+                    this.setState({loaded: rows});
                 }
             );
+    }
+
+    getTrProps = (state, rowInfo, instance) => {
+        if (rowInfo) {
+            return {
+
+                style: {
+                    background: rowInfo.original.color,
+                }
+            }
+        }
+        return {};
     }
 
     render() {
         return (
             <div className="MyTable">
-                <DropdownButton id="dropdown-basic-button" title="Фильтр">
-                    <div className="droplist" onClick={() => {
-                        this.showFiltered("any")
-                    }}>Показать все
+
+                <div className="row">
+                    <div className="column left">
+                        <DropdownButton id="dropdown-basic-button" title="Фильтр">
+                            <div className="droplist" onClick={() => {
+                                this.showFiltered("any")
+                            }}>Показать все
+                            </div>
+                            <div className="droplist" onClick={() => {
+                                this.showFiltered("red")
+                            }}>Показать срочные
+                            </div>
+                            <div className="droplist" onClick={() => {
+                                this.showFiltered("common")
+                            }}>Показать несрочные
+                            </div>
+                            <div className="droplist" onClick={() => {
+                                this.showFiltered("dated")
+                            }}>Показать просроченные
+                            </div>
+                            <div className="droplist" onClick={() => {
+                                this.showFiltered("green")
+                            }}>Показать исполненные
+                            </div>
+                        </DropdownButton>
                     </div>
-                    <div className="droplist" onClick={() => {
-                        this.showFiltered("red")
-                    }}>Показать срочные
+                    <div className="column right">
+
+                        <FormGroup controlId="search" className="search-inp" bsSize="large">
+                            <FormControl
+                                placeholder="Найти"
+                                type="text"
+                                value={this.state.search}
+                                onChange={this.handleChange}
+                            />
+                        </FormGroup>
                     </div>
-                    <div className="droplist" onClick={() => {
-                        this.showFiltered("common")
-                    }}>Показать несрочные
-                    </div>
-                    <div className="droplist" onClick={() => {
-                        this.showFiltered("dated")
-                    }}>Показать просроченные
-                    </div>
-                    <div className="droplist" onClick={() => {
-                        this.showFiltered("green")
-                    }}>Показать исполненные
-                    </div>
-                </DropdownButton>
-                <MDBDataTable
-                    bordered
+                </div>
+
+
+                <ReactTable
                     data={this.state.data}
-                    sorting={false}
+                    columns={[
+                        {
+                            Header: 'Дата получения',
+                            accessor: "receiving_date",
+                            sortMethod: (a, b) => {
+                                if (a === b) {
+                                    return 0;
+                                }
+                                const aReverse = a.split("/").reverse().join("/");
+                                const bReverse = b.split("/").reverse().join("/");
+                                return aReverse > bReverse ? 1 : -1;
+                            }
+                        },
+                        {
+                            Header: 'Наименование суда',
+                            accessor: "court",
+                        }, {
+                            Header: 'Судья (ФИО)',
+                            accessor: "judge",
+                        }, {
+                            Header: "Истец",
+                            accessor: "plaintiff",
+                        }, {
+                            Header: "Ответчик",
+                            accessor: "defendant",
+                        }, {
+                            Header: "Дата исполнения",
+                            accessor: "execution_date",
+                            sortMethod: (a, b) => {
+                                if (a === b) {
+                                    return 0;
+                                }
+                                const aReverse = a.split("/").reverse().join("/");
+                                const bReverse = b.split("/").reverse().join("/");
+                                return aReverse > bReverse ? 1 : -1;
+                            }
+                        }, {
+                            Header: "Изменить",
+                            accessor: "edit",
+                        },
+                    ]}
+                    getTrProps={this.getTrProps}
+                    defaultPageSize={10}
+                    className=" -highlight"
+
                 />
                 <Button onClick={this.handleAdd} className="myButton">Добавить</Button>
             </div>
